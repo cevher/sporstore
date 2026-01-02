a752306a4bcf4de2c1b6fb1574acf0ba9c401419
cevher <efeozden@gmail.com>
2026-01-02 22:18:28 +0300

    feat: implement shopping cart functionality with product variation support

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -141,3 +141,17 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# Email SMTP Configuration
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'pazocak@gmail.com'
+EMAIL_HOST_PASSWORD = 'qlcw khns utwy kurp'
